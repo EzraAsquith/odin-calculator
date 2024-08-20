220068fbf170aa8018ccf8f5140ceb68857e9ca3
Ezra Asquith <ezra.asquith@gmail.com>
2024-08-19 20:08:33 -0700

    Added buttons and basic JS for those buttons

---
+++ script.js
@@ -0,0 +1,16 @@
+const numberField = document.querySelector("#item")
+
+const oneButton = document.querySelector(".one")
+const twoButton = document.querySelector(".two")
+const threeButton = document.querySelector(".three")
+const fourButton = document.querySelector(".four")
+const fiveButton = document.querySelector(".five")
+const sixButton = document.querySelector(".six")
+const sevenButton = document.querySelector(".seven")
+const eightButton = document.querySelector(".eight")
+const nineButton = document.querySelector(".nine")
+const zeroButton = document.querySelector(".zero")
+
+oneButton.addEventListener("click", () => {
+    numberField.textContent.
+})
