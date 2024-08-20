ef740c2a4b8d48d41b69d05c5e0947ceecdbfb64
Ezra Asquith <ezra.asquith@gmail.com>
2024-08-19 20:30:52 -0700

    Added button functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const numberField = document.querySelector("#item")
+const numberField = document.querySelector(".item")
+const clearButton = document.querySelector(".clear")
 
 const oneButton = document.querySelector(".one")
 const twoButton = document.querySelector(".two")
@@ -11,6 +12,36 @@ const eightButton = document.querySelector(".eight")
 const nineButton = document.querySelector(".nine")
 const zeroButton = document.querySelector(".zero")
 
+clearButton.addEventListener("click", () => {
+    numberField.value = ""
+})
 oneButton.addEventListener("click", () => {
-    numberField.textContent.
+    numberField.value += "1"
+})
+twoButton.addEventListener("click", () => {
+    numberField.value += "2"
+})
+threeButton.addEventListener("click", () => {
+    numberField.value += "3"
+})
+fourButton.addEventListener("click", () => {
+    numberField.value += "4"
+})
+fiveButton.addEventListener("click", () => {
+    numberField.value += "5"
+})
+sixButton.addEventListener("click", () => {
+    numberField.value += "6"
+})
+sevenButton.addEventListener("click", () => {
+    numberField.value += "7"
+})
+eightButton.addEventListener("click", () => {
+    numberField.value += "8"
+})
+nineButton.addEventListener("click", () => {
+    numberField.value += "9"
+})
+zeroButton.addEventListener("click", () => {
+    numberField.value += "0"
 })
